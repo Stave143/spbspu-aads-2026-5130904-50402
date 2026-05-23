869da6fb97c23c5bd9be70c81c7b8120ba898439
Stave143 <staveplay@mail.ru>
2026-05-23 17:13:19 +0300

    init test-main, test-queue, test-stack

---
+++ permyakov.sergey/S2/test-queue.cpp
@@ -0,0 +1,8 @@
+#include <boost/test/unit_test.hpp>
+#include "queue.hpp"
+
+using namespace permyakov;
+
+BOOST_AUTO_TEST_SUITE(QueueTests)
+
+BOOST_AUTO_TEST_SUITE_END()
---
+++ permyakov.sergey/S2/test-stack.cpp
@@ -0,0 +1,8 @@
+#include <boost/test/unit_test.hpp>
+#include "stack.hpp"
+
+using namespace permyakov;
+
+BOOST_AUTO_TEST_SUITE(StackTests)
+
+BOOST_AUTO_TEST_SUITE_END()
