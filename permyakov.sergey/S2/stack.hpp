38aff36b848400617732d606fb9603ec6de3f0a8
Stave143 <staveplay@mail.ru>
2026-05-23 17:08:49 +0300

    implement class stack.hpp

--- permyakov.sergey/S2/stack.hpp
+++ permyakov.sergey/S2/stack.hpp
@@ -14,6 +14,31 @@ namespace permyakov
     private:
       List< T > data;
   };
+
+  template< class T > void queue< T >::push(const T value)
+  {
+    data.push_front(value);
+  }
+
+  template< class T > void queue< T > ::pop()
+  {
+    data.pop_front();
+  }
+
+  template< class T > T queue< T >::top()
+  {
+    return data.front();
+  }
+
+  template< class T > size_t queue< T >::size()
+  {
+    return data.size();
+  }
+
+  template< class T > bool queue< T >::isEmpty()
+  {
+    return !size();
+  }
 }
 
 #endif
