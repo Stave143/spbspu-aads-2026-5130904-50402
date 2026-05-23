4315665f99b4fcc316f0ca8cfd5f115cb5f814c8
Stave143 <staveplay@mail.ru>
2026-05-23 16:40:34 +0300

    implement class queue

--- permyakov.sergey/S2/queue.hpp
+++ permyakov.sergey/S2/queue.hpp
@@ -1,6 +1,7 @@
 #ifndef QUEUE_HPP
 #define QUEUE_HPP
 #include "iterators.hpp"
+#include <cstddef>
 
 namespace permyakov
 {
@@ -13,21 +14,32 @@ namespace permyakov
     bool isEmpty();
     private:
       List< T > data;
-  }
+  };
 
   template< class T > void queue< T >::push(const T value)
   {
     LIter< T > endIter = date.end();
-    date.push_back(endIter, value);
+    data.insert_after(endIter, value);
   }
 
   template< class T > void queue< T > ::pop()
   {
-    LIter< T > iter = date.begin();
-    for (size_t i = 1; i < date.size(); ++i) {
-      ++iter;
-    }
-    date.erase_after(iter);
+    data.pop_front();
+  }
+
+  template< class T > T queue< T >::front()
+  {
+    return data.front();
+  }
+
+  template< class T > size_t queue< T >::size()
+  {
+    return data.size();
+  }
+
+  template< class T > bool queue< T >::isEmpty()
+  {
+    return !size();
   }
 }
 
